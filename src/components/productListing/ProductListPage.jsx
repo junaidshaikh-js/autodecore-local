cbb3ae98fc447ede9bd49b6e4cd09ceef0213260
Junaid <sj.shaikhjunaid@gmail.com>
2022-03-06 19:20:02 +0530

    [fix] change class to className

--- src/components/productListing/ProductListPage.jsx
+++ src/components/productListing/ProductListPage.jsx
@@ -4,9 +4,9 @@ import { useStateContext } from "../../context/state-context";
 import { Filter } from "./Filter";
 import { ProductCard } from "./ProductCard";
 import { getProducts } from "../../utils/server-request";
+import { Loader } from "../loader";
 
 import "./product-listing.css";
-import { Loader } from "../loader";
 
 export function ProductListingPage() {
   const [isLoading, setIsloading] = useState(false);
@@ -28,12 +28,12 @@ export function ProductListingPage() {
         <>
           <Filter />
 
-          <section class="products-section p-1 my-1 mx-1 flex-grow bg-white">
-            <p class="h5">Showing all products ({products.length})</p>
+          <section className="products-section p-1 my-1 mx-1 flex-grow bg-white">
+            <p className="h5">Showing all products ({products.length})</p>
 
-            <div class="product-container grid mt-2">
+            <div className="product-container grid mt-2">
               {products.map((product) => {
-                return <ProductCard product={product} />;
+                return <ProductCard key={product.id} product={product} />;
               })}
             </div>
           </section>
