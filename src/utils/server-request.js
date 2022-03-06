f227661464cdce72ea5477c51e6faaee180ef650
Junaid <sj.shaikhjunaid@gmail.com>
2022-03-06 19:21:05 +0530

    add getCartProduct and addToCart server request

--- src/utils/server-request.js
+++ src/utils/server-request.js
@@ -1,6 +1,7 @@
+/* eslint-disable eqeqeq */
 import axios from "axios";
 
-import { PRODUCTS_URL } from "./constant";
+import { PRODUCTS_URL, CART_URL } from "./constant";
 
 export const getProducts = async (dispatch, setLoading) => {
   try {
@@ -14,5 +15,40 @@ export const getProducts = async (dispatch, setLoading) => {
     setLoading(false);
   } catch (error) {
     throw new Error("Products can not be loaded");
+  }
+};
+
+export const getCartProducts = async (dispatch) => {
+  try {
+    const res = await axios.get(CART_URL);
+
+    if (res.status == 200) {
+      dispatch({ type: "SET_CART", payload: res.data });
+    }
+  } catch (error) {
+    throw new Error("Products can not be loaded");
+  }
+};
+
+export const addItemToCart = async (dispatch, product, state, isLoading) => {
+  try {
+    isLoading(true);
+    const res = await axios({
+      method: "post",
+      url: CART_URL,
+      data: {
+        ...product,
+        cartQty: 1,
+      },
+    });
+
+    dispatch({
+      type: "SET_CART",
+      payload: [...state.productsInCart, res.data],
+    });
+
+    isLoading(false);
+  } catch (error) {
+    throw new Error("failed! try again");
   }
 };
