5484e49f375bbf9ea55a86908fa51b9af1feeac1
Junaid <sj.shaikhjunaid@gmail.com>
2022-03-07 16:00:42 +0530

    add wishlist api url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,3 +2,6 @@ export const PRODUCTS_URL =
   "https://621bad4a768a4e10209a1517.mockapi.io/api/products";
 
 export const CART_URL = "https://621bad4a768a4e10209a1517.mockapi.io/api/cart";
+
+export const WISHLIST_URL =
+  "https://621bad4a768a4e10209a1517.mockapi.io/api/wishlist";
