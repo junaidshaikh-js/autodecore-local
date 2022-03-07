33c49c8ed7908100e7aa4eaf750cb38b21745797
Junaid <sj.shaikhjunaid@gmail.com>
2022-03-07 15:59:54 +0530

    [feat] add to wishlist button on product card

--- src/components/productListing/ProductCard.jsx
+++ src/components/productListing/ProductCard.jsx
@@ -1,9 +1,17 @@
+import { FaRegHeart, FaHeart } from "react-icons/fa";
+import { useState } from "react";
+
 import { ProductHeader } from "./ProductHeader";
 import { BtnIcon } from "../buttons";
 import { ProductCardPrice } from "./ProductCardPrice";
 import { AddToCartBtn } from "./AddToCartBtn";
+import { useStateContext } from "../../context";
+import { addToWishlist } from "../../utils";
 
 export function ProductCard({ product }) {
+  const { state, dispatch } = useStateContext();
+  const [isUpdating, setIsUpdating] = useState(false);
+
   return (
     <article className="card card-vertical p-1 border-m product-card card-icon">
       <div className="card-image flex align-center">
@@ -28,13 +36,16 @@ export function ProductCard({ product }) {
         <div className="card-btn-container">
           <AddToCartBtn product={product} />
 
-          {/* <BtnComplementary cnames="w-100 btn-addtocart">
-            <i className="fas fa-shopping-cart"></i>
-            <span>Add to Cart</span>
-          </BtnComplementary> */}
-
-          <BtnIcon cnames={["btn", "icon-btn-primary", "btn-wishlist"]}>
-            <i className="far fa-heart" title="add to wishlist"></i>
+          <BtnIcon
+            cnames={["btn", "icon-btn-primary", "btn-wishlist"]}
+            onClick={() => addToWishlist(dispatch, product, setIsUpdating)}
+            disabled={isUpdating}
+          >
+            {product.inWishList ? (
+              <FaHeart color="red" fontSize="18px" />
+            ) : (
+              <FaRegHeart color="red" fontSize="18px" />
+            )}
           </BtnIcon>
         </div>
       </div>
