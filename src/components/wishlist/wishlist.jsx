45dc21f6a3d8dd9ca13ae5f19623b8170daf9daf
Junaid <sj.shaikhjunaid@gmail.com>
2022-03-08 13:39:45 +0530

    [fix] netlify deploy issue

--- src/components/wishlist/wishlist.jsx
+++ src/components/wishlist/wishlist.jsx
@@ -1,14 +1,9 @@
-import { useStateContext } from "../../context";
 import { WishUser } from "./WishUser";
 import { WishlistContainer } from "./WishlistContainer";
 
 import "./wishlist.css";
 
 export function Wishlist() {
-  const {
-    state: { productsInWishList },
-  } = useStateContext();
-
   return (
     <main className="wishlist-main flex flex-column">
       <WishUser />
