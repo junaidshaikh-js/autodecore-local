d694c3ea1ee75425754e910ceb29b0be1d718759
Junaid <sj.shaikhjunaid@gmail.com>
2022-03-11 12:08:57 +0530

    [update] remove product description, add line clamp to title

--- src/components/productListing/ProductHeader.jsx
+++ src/components/productListing/ProductHeader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 export function ProductHeader({ name, soldBy, rating }) {
   return (
     <>
-      <p className="h6 card-title">{name}</p>
+      <p className="txt-sm card-title">{name}</p>
       <small className="txt-secondary">{soldBy}</small>
       <div className="product-rating my-sm p-sm">
         {rating} <i className="fas fa-star"></i>
