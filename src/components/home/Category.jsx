6bafa592327664709a175356e99f26064874ea5d
Junaid <sj.shaikhjunaid@gmail.com>
2022-03-13 20:10:36 +0530

    [feat] add link to navigate from home page to product page

--- src/components/home/Category.jsx
+++ src/components/home/Category.jsx
@@ -1,16 +1,34 @@
+import { Link } from "react-router-dom";
+import { useStateContext } from "../../context";
+
 export function Category({ name, sources }) {
+  const { dispatch } = useStateContext();
   return (
     <div className="bg-white my-1">
       <section className="category-section  txt-center p-sm pb-1 ">
         <header className="py-1">
           <h2 className="h3">{name}</h2>
         </header>
+
         <div className="category-container gap-1 m-sm">
           {sources.map(({ name, image }) => {
             return (
-              <figure key={name}>
-                <img src={image} alt="" />
-              </figure>
+              <Link to="/products">
+                <figure
+                  key={name}
+                  onClick={() => {
+                    dispatch({
+                      type: "FILTER_BY_CATEGORY",
+                      payload: {
+                        isChecked: true,
+                        value: name.toLowerCase(),
+                      },
+                    });
+                  }}
+                >
+                  <img src={image} alt="" />
+                </figure>
+              </Link>
             );
           })}
         </div>
