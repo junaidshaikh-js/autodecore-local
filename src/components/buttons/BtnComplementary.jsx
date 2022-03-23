1c235bf388714ea361e0d0d4137ea97fc9cabda6
Junaid <sj.shaikhjunaid@gmail.com>
2022-03-23 09:41:06 +0530

    add default button type to button

--- src/components/buttons/BtnComplementary.jsx
+++ src/components/buttons/BtnComplementary.jsx
@@ -1,8 +1,17 @@
 import "./button.css";
 
-export function BtnComplementary({ children, onClick, cnames }) {
+export function BtnComplementary({
+  children,
+  onClick,
+  cnames,
+  type = "button",
+}) {
   return (
-    <button className={"btn btn-complementary " + cnames} onClick={onClick}>
+    <button
+      className={"btn btn-complementary " + cnames}
+      onClick={onClick}
+      type={type}
+    >
       {children}
     </button>
   );
